8b953e44540b210b6f979146955e075b63bb6f7e
Quentin Vannelle <quentin.vannelle@hotmail.fr>
2022-10-21 21:26:29 +0200

    implémentation accordéon dans la page à propos

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,6 +3,7 @@ import Footer from '../composants/Footer'
 import Header from '../composants/Header'
 import paysage2 from '../assets/paysage2.png'
 import flèche_haut from '../assets/flèche_haut.png'
+import Accordion from '../composants/Accordion';
 
 export default function About() {
   return (
@@ -10,26 +11,12 @@ export default function About() {
     <div className='Section1'>
       <img src={paysage2} alt="paysage_section_à_propos" srcset="" />
     </div>
-    <div>
-      <ul>
-        <li className='Dropdown'>
-        <h4 className='Rectangle4'>Fiabilité <button><img src={flèche_haut} alt="flèche_haut"  /></button></h4>
-        <p className='Dropdown-open'>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.</p>
-        </li>
-        <li className='Dropdown'>
-        <h4 className='Rectangle4'>Respect <button><img src={flèche_haut} alt="flèche_haut"  /></button></h4>
-        <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
-        </li>
-        <li className='Dropdown'>
-        <h4 className='Rectangle4'>Service <button><img src={flèche_haut} alt="flèche_haut"  /></button></h4>
-        <p>Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.</p>
-        </li>
-        <li className='Dropdown'>
-        <h4 className='Rectangle4'>Responsabilité <button><img src={flèche_haut} alt="flèche_haut"  /></button></h4>
-        <p>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.</p>
-        </li>
-      </ul>
-    </div>
+    <div className='Dropdown'>
+      <Accordion titre={"Fiabilité"} contenu={"Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."}> </Accordion>
+      <Accordion titre={"Respect"} contenu={"La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."}></Accordion>
+      <Accordion titre={"Service"} contenu={"Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."}></Accordion>
+      <Accordion titre={"Responsabilité"} contenu={"Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."}></Accordion>
+  </div>
     <Footer/>
     </>
   )
